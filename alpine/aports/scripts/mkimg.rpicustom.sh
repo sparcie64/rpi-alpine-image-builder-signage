ffedd1b9c34c0fde8efef9cb8270d1b20f41bb08
SPARCie <sparcie@sparcie.ml>
2025-11-23 14:45:26 +0100

    typo

--- alpine/aports/scripts/mkimg.rpicustom.sh
+++ alpine/aports/scripts/mkimg.rpicustom.sh
@@ -3,5 +3,5 @@ profile_rpicustom() {
     title="PISignage by SPARCie"
     desc="Built by Raspberry Pi Alpine Image Builder"
     apkovl="genapkovl-rpicustom.sh"
-    apks="dbus dbus-x11 bash python3 tmux xorg-server xf86-input-libinput xinit eudev mesa-dri-galliumchromium nano vim bash-completion"
+    apks="dbus dbus-x11 bash python3 tmux xorg-server xf86-input-libinput xinit eudev mesa-dri-gallium chromium nano vim bash-completion"
 }
